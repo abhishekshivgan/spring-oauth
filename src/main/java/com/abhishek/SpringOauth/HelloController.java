2b47c978d5e29075b53854c9ca82f0b9dda8635e
Abhishek <abhisheklitlost@gmail.com>
2024-11-14 13:06:54 +0530

    Added OAuth2

---
+++ src/main/java/com/abhishek/SpringOauth/HelloController.java
@@ -0,0 +1,13 @@
+package com.abhishek.SpringOauth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/")
+	public String greet() {
+		return "Hello, Abhishek";
+	}
+}
